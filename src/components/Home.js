5f7327c4bc8b637407b168b021c414cb1eae2a7c
Rudresh <rudreshgs>
2021-07-13 09:20:15 +0530

    Initial commit.

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = (props) => {
+    return (
+        <div className='home_heading'>
+            <h1> User Authentication with Redux  </h1>
+        </div>
+    )
+}
+
+export default Home
